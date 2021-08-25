ab56e5deef28723c85e759f3892b2fb029edc3b5
RVÐ <triangle1232009@hotmail.com>
2021-08-25 01:39:39 -0400

    Personnae[acteur] physic proccess -> calcul de mouvement selon gravité et vitesse

---
+++ monPremierGoDot/Scripts/Personnages.gd
@@ -0,0 +1,63 @@
+extends Acteur
+
+class_name Personnage
+# Declare member variables here. Examples:
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+#func _process(delta: float) -> void:
+#	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	var est_saut_interompu = Input.is_action_just_released("Saut") and velocite.y < 0.0
+	var direction = avoir_DirectionV2()
+	velocite = velocite_mouvement(velocite, direction, vitesseMin, est_saut_interompu)
+	move_and_slide(velocite, FLOOR_NORMAL)
+	
+	print(velocite)
+		#Animations
+	if velocite.x != 0:
+		$AnimationPlayer.play("Courrir")
+	else:
+		$AnimationPlayer.play("Repos")
+		
+	if velocite.y < 0.0:
+		$AnimationPlayer.play("Courrir")
+	else:
+		$AnimationPlayer.play("Repos")
+	
+		
+	
+func avoir_directionX() -> float:
+	return Input.get_action_strength("Droite") - Input.get_action_strength("Gauche")
+	
+func avoir_directionY() -> float:
+	var directionY = -1.5 if Input.is_action_just_pressed("Saut") and is_on_floor() else 1.0
+	
+	return directionY
+
+
+func avoir_DirectionV2() -> Vector2:
+	return Vector2( avoir_directionX(), avoir_directionY() )
+	
+func velocite_mouvement(
+		velocite_lineraire : Vector2,
+		direction: Vector2,
+		vitesse: Vector2,
+		est_saut_interompu: bool
+	) -> Vector2:
+	var n_velocite = velocite_lineraire
+	n_velocite.x = vitesse.x * direction.x
+	n_velocite.y += gravite * get_physics_process_delta_time()
+	if direction.y == -1.5:
+		n_velocite.y = vitesse.y * direction.y
+	if est_saut_interompu:
+		n_velocite.y = 0.0
+	return 	n_velocite
--- monPremierGoDot/Scripts/Principal.gd
+++ monPremierGoDot/Scripts/Principal.gd
@@ -6,18 +6,14 @@ extends Node
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Personnage/AnimationPlayer.play("Repos")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	print(delta)
-#c
-func _physics_process(delta):
-	if Input.is_action_pressed("Droite"):
-		$Personnage/AnimationPlayer.play("Courrir")
-	if Input.is_action_just_released("Droite"):
-		$Personnage/AnimationPlayer.play("Repos")
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	print(delta)
+##c
+#func _physics_process(delta):
+# pass
