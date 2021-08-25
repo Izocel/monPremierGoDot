3babd77637f5499b2936f9db53756f3d77ab5aba
RVÐ <triangle1232009@hotmail.com>
2021-08-24 22:18:36 -0400

    Ajoute animation synchroniser avec boite colision gloabale

--- monPremierGoDot/Scripts/Principal.gd
+++ monPremierGoDot/Scripts/Principal.gd
@@ -8,12 +8,16 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Personnage/AnimationPlayer.play("Repos")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	print(delta)
-#
-#func _physics_process(delta):
-#	print(delta)
+#c
+func _physics_process(delta):
+	if Input.is_action_pressed("Droite"):
+		$Personnage/AnimationPlayer.play("Courrir")
+	if Input.is_action_just_released("Droite"):
+		$Personnage/AnimationPlayer.play("Repos")
