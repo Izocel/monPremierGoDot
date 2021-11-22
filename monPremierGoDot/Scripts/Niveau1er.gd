fb35f76ffe5e569dfa378f6b1c0b2dc6871d8905
RVÐ <triangle1232009@hotmail.com>
2021-11-22 11:57:52 -0500

    ## Création de tleset invisible pour portail/mur
    Co-authored-by: Allan Morissette (2042858) <2042858@etudiant.cegepvicto.ca>

--- monPremierGoDot/Scripts/Niveau1er.gd
+++ monPremierGoDot/Scripts/Niveau1er.gd
@@ -2,10 +2,11 @@ extends Node
 
 class_name Niveau
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
+var chemin_scene_precedente = "res://Scripts/Niveau1er.gd"
+var chemin_scene_suivante = "res://Scripts/Niveau2e.gd"
+var portail
+var collision
+var joueur
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +14,27 @@ func _ready() -> void:
 	$ColorRect.fondu_sortie()
 
 
-func _process(delta: float) -> void:
+############### Fonction d'action  ###############
 	
+func collision_portail_niveau(collision):
+	# Si il y a une collision avec le prtail
+	if(collision):
+		# Initier le corps de l'objet collisionné	
+		joueur = collision.collider
+		# Si l'objet qui est entré en collision est un portail
+		changement_scene(joueur)
+	
+func changement_scene(joueur) -> void:
+					
+	if(portail.is_in_group("portail_suivant")):
+		get_tree().change_scene(chemin_scene_suivante)
+	
+	
+################ Fonction principale #################
+
+func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta):
+	pass
+
