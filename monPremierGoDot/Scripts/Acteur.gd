9d92c815be78ecf396634b520fa26e0cd1a277dd
RVÐ <triangle1232009@hotmail.com>
2021-11-22 18:13:45 -0500

    # Préparation portail pour alan
    - +Ménage de script inutile ou vide

--- monPremierGoDot/Scripts/Acteur.gd
+++ monPremierGoDot/Scripts/Acteur.gd
@@ -10,22 +10,21 @@ export var vitesseMin = Vector2(333.0,333.0)
 export var gravite = 500.0
 var velocite = Vector2.ZERO
 
-func check_colision_portail_suivant() -> void:
+func check_colision_portail() -> void:
 
 	var last_slide = get_slide_count() - 1
-
+	
 	if(last_slide >=  0):
 		var colision  = get_slide_collision(last_slide)
 		
 		if(colision):
 			var collider = colision.collider
 		
-			if(collider.is_in_group("PortailSuivant")):
-				print(collider)
-				print("le portail changeras de scene")
+			if(collider.is_in_group("Portail")):
+				print("le portail doit tenter de téléporté l'acteur")
 				pass
 
 
 func _physics_process(_delta: float) -> void:
-	check_colision_portail_suivant()
+	check_colision_portail()
 	pass
---
+++ monPremierGoDot/Scripts/Portail.gd
@@ -0,0 +1,40 @@
+extends TileMap
+
+class_name Portail
+func get_class(): return "Portail"
+
+export(PackedScene) var scene_a_charger
+export(Array) var groupes_approuver = ["Acteur", "Personnage"]
+
+
+func as_right_of_teleportation(body_class:String) -> bool:
+	
+	if(1):
+		#si le body est dans une des classe autorisées
+		return true
+	
+	return false
+	
+
+func teleport_body(body:Node) -> void:
+	
+    # Le body sera un Acteur ou un personnage ? C'est la srprisssseeeeeee !!!!
+	print(body.get_class())
+	var body_class = body.get_class()
+	var as_right = false
+	
+	if( as_right_of_teleportation( body_class )):
+
+		#si le body est dans une des classe autorisées
+		print("as right of passage --> TELEPORT")
+		as_right = true
+
+	if(as_right):
+		charger_scene();
+
+func charger_scene() -> void:
+
+	var chemin_scene = scene_a_charger.get_path()
+
+	if(chemin_scene):
+		get_tree().change_scene(chemin_scene)
